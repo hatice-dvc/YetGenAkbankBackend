1912f30d62be95258aee3b8fc3d302db93387f7d
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-10-28 16:05:05 +0300

    [Feat] Domain Layer Created

---
+++ Lecture_7/Core/Week_7_1.Domain/Common/IModifiedByEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_7_1.Domain.Common
+{
+    internal interface IModifiedByEntity
+    {
+    }
+}

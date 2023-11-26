55c279818ccfa133f8fdb14f0bb18483424effaf
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-26 15:59:24 +0300

    [Feat] Create DbContext

---
+++ Week_11_1/Infrastructure/Week_11_1.Persistance/Contexts/PerfectAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_11_1.Domain.Entities;
+
+namespace Week_11_1.Persistance.Contexts
+{
+    public class PerfectAppDbContext : DbContext
+    {
+        public DbSet<BankAccount> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("PerfectApp");
+        }
+    }
+}

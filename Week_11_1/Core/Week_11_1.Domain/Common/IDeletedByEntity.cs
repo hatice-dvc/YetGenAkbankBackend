71a8ba711ce9fb7128e72e2a55c316a1afe42400
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-26 15:58:00 +0300

    [Feat] Domain Layer Created

---
+++ Week_11_1/Core/Week_11_1.Domain/Common/IDeletedByEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_11_1.Domain.Common
+{
+    public interface IDeletedByEntity
+    {
+        public string? DeletedByUserId { get; set; }
+        public DateTimeOffset? DeletedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Week_11_1/Core/Week_11_1.Domain/Entities/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_11_1.Domain.Common;
+
+namespace Week_11_1.Domain.Entities
+{
+    public class BankAccount : EntityBase<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}

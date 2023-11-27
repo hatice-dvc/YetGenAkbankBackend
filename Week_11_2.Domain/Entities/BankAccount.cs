849b84d77397d19ca7a0d7f5b8811fe8e52ace6d
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-11-27 17:01:19 +0300

    [Feat] Domain Layer Created

---
+++ Week_11_2.Domain/Entities/BankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class BankAccount
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+}
+
